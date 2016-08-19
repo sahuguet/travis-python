2d9f991d054d6b77bf8e0b31ae7689d9f8f7ea46
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2016-08-19 11:53:32 -0400

    random test

--- test_sample.py
+++ test_sample.py
@@ -1,13 +1,17 @@
 """Some simple tests."""
 import pytest
+import random
+from datetime import datetime
+random.seed(datetime.now())
+
 
 func = lambda x: x + 1
 
 def test_answer_0():
-    assert func(3) == 5
+    assert random.choice([True, False]) == True
 
 def test_answer_1():
-    assert func(3) == 4
+    assert random.choice([True, False]) == True
 
 def test_answer_2():
-    assert func(3) == 5
+    assert random.choice([True, False]) == True
