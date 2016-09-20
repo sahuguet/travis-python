cdedae39aa9ac9167c5c4d4d692d6f05bf130fc8
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2016-09-20 00:23:46 -0400

    new way of fetching TravisCI jobs

--- homework-grading.py
+++ homework-grading.py
@@ -15,9 +15,11 @@ GIT_COMMIT_FROM_SHA = "https://api.github.com/repos/" + REPO + "/commits/%s"
 TRAVIS_LOG_FROM_ID = "https://api.travis-ci.org/jobs/%d/logs"
 TRAVIS_BUILDS_URL = "https://api.travis-ci.org/repos/%s/builds" % REPO
 TRAVIS_LOG_S3 = "https://s3.amazonaws.com/archive.travis-ci.org/jobs/%d/log.txt"
+TRAVIS_BUILD_URL = "https://api.travis-ci.org/repos/" + REPO + "/builds?ids=%d"
 
 GIT_USER = 'sahuguet'
 GITHUB_TOKEN = 'd621ee8931b06586b015266ea682a6bbb3730d2f'
+
 
 def get_travis_url_from_git(sha):
     """Grabs the Travis URL from the git commit data."""
@@ -49,10 +51,10 @@ def get_pytest_report_from_s3(job_id, user):
         print >> sys.stderr, "wrong S3 id for user %s." % user
         return pytest_report
     url = TRAVIS_LOG_S3 % int(job_id)
-    #print >> sys.stderr, url
+    print >> sys.stderr, url
     response = requests.get(url)
-    log_data = response.text
-    pattern = re.compile('<MQkrXV>[^{]+([{].*[}]{3})', re.MULTILINE)
+    log_data = response.text.replace('\n', ' ').replace('\r', ' ')
+    pattern = re.compile('<MQkrXV>[^{]+([{].*[}]{2,3}) ', re.MULTILINE)
     match = pattern.search(log_data)
     if match:
         pytest_report = match.group(1)
@@ -80,10 +82,23 @@ def get_travis_jobid(builds, build_id):
         if build['id'] == build_id:
             return int(build['job_ids'][0])
 
+def get_travis_jobid2(build_id):
+    url = TRAVIS_BUILD_URL % build_id
+    print >> sys.stderr, url
+    headers = {'User-Agent': 'MyClient/1.0.0', 'Accept': 'application/vnd.travis-ci.2+json'}
+    response = requests.get(url, headers=headers)
+    data = response.text
+    builds = json.loads(data)['builds']
+    print >> sys.stderr, json.dumps(builds, indent=2, sort_keys=True)
+    return builds[0]['job_ids'][0]
+
 """
 curl -H "User-Agent: MyClient/1.0.0" -H "Accept: application/vnd.travis-ci.2+json" \
 https://api.travis-ci.org/repos/startup-systems/static/builds
 """
+
+#print get_pytest_report_from_s3(161120715, 'user')
+#sys.exit(-1)
 
 if __name__ == '__main__':
     # We get all the pull-requests from the repo.
@@ -111,7 +126,8 @@ if __name__ == '__main__':
         print >> sys.stderr, "Build_id %s for user %s." % (build_id, user)
 
         # Get TravisCI jobid from build_id.
-        job_id = get_travis_jobid(TRAVIS_BUILDS, build_id)
+        #job_id = get_travis_jobid(TRAVIS_BUILDS, build_id)
+        job_id = get_travis_jobid2(build_id)
         print >> sys.stderr, job_id
 
         SUBMISSIONS.append({'user': user, 'sha': sha, 'timestamp': timestamp,
